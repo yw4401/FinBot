7cdb014f215a984f36e410903ed39f9179e234f0
Shilong Dai <shilong@uchicago.edu>
2023-05-02 03:13:13 +0000

    Change temperature for koala, and add vicuna notebook

---
+++ scripts/setup_vicuna_raw.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "\nSetting Up New Environment for Vicuna\n"
+
+conda create -n vicuna python=3.10 -y
+conda run -n vicuna conda install ipykernel -y
+conda run -n vicuna conda install pytorch torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c "nvidia/label/cuda-11.7.1" -y
+conda run -n vicuna conda install -c conda-forge ipywidgets -y
+pip install fschat
+
+echo "\nDownloading Plain Llama Weights\n"
+gsutil cp -r gs://model-weights-null/vicuna-7b /home/jupyter
+
+
+echo "\nIf there were no errors, Vicuna Notebook at notebooks/llama_vicuna.ipynb should be ready to go. Make sure that you have at least a 16G of GPU memory and a reasonable amount of regular memory.\n"
+
+
