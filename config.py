5a9f40815d018c0f666fc7bf5c2a194e859c6c6b
Shilong Dai <shilongdai@gmail.com>
2023-09-05 20:04:01 -0400

    refractored consolidate_scraper.py

---
+++ config.py
@@ -0,0 +1,12 @@
+GCP_PROJECT = "msca310019-capstone-f945"
+
+# Article Ingestion
+ARTICLE_TARGET_BUCKET = "markdown-converged"
+ARTICLE_CONVERT_META_BUCKET = "meta-info"
+ARTICLE_CONVERT_META_IDX = "scraper-markdown-index.json"
+ARTICLE_BUCKET_IDX = "index.json"
+ARTICLE_CONVERT_CHUNK = 100
+ARTICLE_CONVERT_CNBC_DATE = "%Y-%m-%dT%H:%M:%S%z"
+ARTICLE_CONVERT_REUTER_DATE = "%B %d, %Y %I:%M %p"
+ARTICLE_CONVERT_NYT_DATE = "%Y-%m-%dT%H:%M:%S%z"
+
