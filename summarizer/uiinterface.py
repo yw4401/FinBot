89c0d9fa3198dd6c721728d1bd4f86ef5627a5f9
Yif Wang <yif4401@outlook.com>
2023-10-24 17:08:19 -0500

    intergration of qa and summary to ui

---
+++ summarizer/uiinterface.py
@@ -0,0 +1,7 @@
+def finbot_response(text, period):
+    return {
+        "qa": "Placeholder QA",
+        "summaries": [{"title": "Main Point", "keypoints": ["K1", "K2", "K3"]}, 
+                      {"title": "Lorem Ipsum", "keypoints": ["K1", "K2", "K3"]}]
+    }
+
