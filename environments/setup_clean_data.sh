52cd83552b1855a75ea9a9b4cc67259416b2511c
Shilong Dai <sdai@viperfish.net>
2023-10-31 18:58:50 -0500

    Adds bigquery clean_data.py with environment

---
+++ environments/setup_clean_data.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "\nSetting Up New Environment for Clean Data Pipeline\n"
+
+conda create -n clean_data python=3.9 -y
+conda run -n clean_data conda install -c conda-forge ipykernel ipywidgets -y
+conda run -n clean_data pip install allennlp allennlp-models
+conda run -n clean_data pip uninstall torch torch-audio torch-vision
+conda run -n clean_data conda install pytorch==1.12.1 torchvision==0.13.1 torchaudio==0.12.1 cudatoolkit=11.6 -c pytorch -c conda-forge -y
+conda run -n clean_data pip install google-cloud-bigquery typing-inspect==0.8.0 typing_extensions==4.5.0 pydantic==1.10.8 networkx
+conda run -n clean_data pip install -U numpy
+
+echo "Activating environment"
+
+conda run -n vicuna ipython kernel install --user --name=clean_data
+
+
+
