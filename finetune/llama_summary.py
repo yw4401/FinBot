61c15e4b5a3187ade8be9f5687a9a70e458f3da1
Shilong Dai <shilongdai@gmail.com>
2023-11-05 16:02:58 -0600

    Fix bug

--- finetune/llama_summary.py
+++ finetune/llama_summary.py
@@ -10,6 +10,7 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     TrainingArguments,
+    AutoConfig,
     HfArgumentParser, )
 from trl import SFTTrainer, DataCollatorForCompletionOnlyLM
 
@@ -81,10 +82,15 @@ def main():
 
     # loading the base model
     with OnDevice(dtype=torch.bfloat16, device="meta", enabled=True):
-        model = AutoModelForCausalLM.from_pretrained(
-            script_args.model_path, use_cache=not train_args.gradient_checkpointing, token=hf_token,
-            torch_dtype=torch.bfloat16, use_flash_attention_2=True, low_cpu_mem_usage=True,
-            cache_dir=script_args.cache_dir)
+        try:
+            model = AutoModelForCausalLM.from_pretrained(
+                script_args.model_path, use_cache=not train_args.gradient_checkpointing, token=hf_token,
+                torch_dtype=torch.bfloat16, use_flash_attention_2=True, low_cpu_mem_usage=True,
+                cache_dir=script_args.cache_dir)
+        except ValueError:
+            model = AutoModelForCausalLM.from_pretrained(
+                script_args.model_path, use_cache=not train_args.gradient_checkpointing, token=hf_token,
+                torch_dtype=torch.bfloat16, use_flash_attention_2=True, cache_dir=script_args.cache_dir)
     if train_args.gradient_checkpointing:
         model.gradient_checkpointing_enable()
 
