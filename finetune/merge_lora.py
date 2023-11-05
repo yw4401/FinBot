77239d1661276ee06fd7ef23b88854f6d22d1e68
Shilong Dai <sdai@viperfish.net>
2023-11-05 10:32:31 -0600

    Adds ability to load deepspeed LORA checkpoints

--- finetune/merge_lora.py
+++ finetune/merge_lora.py
@@ -1,12 +1,11 @@
-from dataclasses import dataclass, field
-from typing import Optional, cast
-
 import torch
-from deepspeed.utils.zero_to_fp32 import load_state_dict_from_zero_checkpoint
-from peft import PeftModel
+from dataclasses import dataclass, field
+from deepspeed.utils.zero_to_fp32 import get_fp32_state_dict_from_zero_checkpoint
+from peft import PeftModel, PeftConfig, get_peft_model
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from transformers import (
     HfArgumentParser, )
+from typing import Optional, cast
 
 
 @dataclass
@@ -16,6 +15,7 @@ class ScriptArguments:
     lora_path: Optional[str] = field(default="./summary-llama-lora")
     cache_dir: Optional[str] = field(default="./transformers")
     model_out: Optional[str] = field(default="./summary-llama")
+    deepspeed: Optional[bool] = field(default=False)
 
 
 if __name__ == "__main__":
@@ -32,7 +32,14 @@ if __name__ == "__main__":
                                                  use_flash_attention_2=True,
                                                  cache_dir=script_args.cache_dir)
     print("Loading LORA")
-    peft_model = PeftModel.from_pretrained(model, script_args.lora_path)
+    if script_args.deepspeed:
+        config = PeftConfig.from_pretrained(script_args.lora_path)
+        peft_model = get_peft_model(model, config)
+        state_dict = get_fp32_state_dict_from_zero_checkpoint(script_args.lora_path)  # already on cpu
+        peft_model.load_state_dict(state_dict)
+    else:
+        peft_model = PeftModel.from_pretrained(model, script_args.lora_path)
+    peft_model = peft_model.to(dtype=torch.float16)
     print("Merging Model")
     merged_model = peft_model.merge_and_unload()
     print("Saving Merged Model")
