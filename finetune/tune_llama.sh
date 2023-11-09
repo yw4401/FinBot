4e8750d72349710cb1571464cb2214d619bc8dbc
Shilong Dai <sdai@viperfish.net>
2023-11-09 00:51:37 -0600

    Adds validation set

--- finetune/tune_llama.sh
+++ finetune/tune_llama.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 gsutil cp gs://scraped-news-article-data-null/fine-tune-summary-train.parquet .
 gsutil cp gs://scraped-news-article-data-null/fine-tune-summary-test.parquet .
-deepspeed llama_summary.py --deepspeed=deepspeed_llama2.json --output_dir="./summary-llama-lora" \
- --per_device_train_batch_size=1 --learning_rate=1e-4 --num_train_epochs=1 --weight_decay=0.1 --warmup_steps=200 \
- --bf16=True --lr_scheduler_type="cosine" --model_path="meta-llama/Llama-2-7b-chat-hf" --remove_unused_columns=False \
- --sample=30000 --save_strategy "no"
+deepspeed tune_summary.py --deepspeed=deepspeed_llama2.json --output_dir="./summary-mistral-lora" \
+ --per_device_train_batch_size=1 --learning_rate=1e-4 --num_train_epochs=4 --weight_decay=0.1 --warmup_steps=200 \
+ --bf16=True --lr_scheduler_type="cosine" --model_path="Open-Orca/Mistral-7B-OpenOrca" --remove_unused_columns=False \
+ --sample=1000000 --save_strategy "epoch" --lora_target "q_proj,k_proj,v_proj,o_proj" --lora_r 16 --lora_alpha 16 \
+ --gradient_checkpointing True --evaluation_strategy="epoch" --logging_strategy="epoch"
