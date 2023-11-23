4257a4217aba8e0ca7183fd68bbdc4819d2e4af9
Shilong Dai <sdai@viperfish.net>
2023-11-23 13:48:55 -0600

    Rename qa script output

--- finetune/tune_llama_qa.sh
+++ finetune/tune_llama_qa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 gsutil cp gs://scraped-news-article-data-null/fine-tune-qa-train.parquet .
-deepspeed tune_qa.py --deepspeed=deepspeed_llama2.json --output_dir="./summary-mistral-lora" \
+deepspeed tune_qa.py --deepspeed=deepspeed_llama2.json --output_dir="./qa-llama-lora" \
  --per_device_train_batch_size=1 --learning_rate=1e-4 --num_train_epochs=2 --weight_decay=0.1 --warmup_steps=200 \
  --bf16=True --lr_scheduler_type="cosine" --remove_unused_columns=False \
  --sample=1000000 --save_strategy "epoch" --gradient_checkpointing True
